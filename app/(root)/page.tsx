87b0e5694af77ec4ad11b60614e939a74bef1c15
Anurag9977 <anuragchoudhury852@gmail.com>
2025-01-10 18:28:37 +0530

    Fixed redirect issue

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,6 @@
-import DashboardChart from "@/components/dashboard/DashboardChart";
-import DashboardStats from "@/components/dashboard/DashboardStats";
-import RecentFiles from "@/components/dashboard/RecentFiles";
-import MainContainer from "@/components/globals/MainContainer";
-import { getAllFiles } from "@/utils/actions";
+import { redirect } from "next/navigation";
 
-async function DashboardPage() {
-  const { documents, totalSize } = await getAllFiles({});
-
-  return (
-    <MainContainer className="h-full py-8 grid lg:grid-cols-10 gap-y-6 gap-x-8">
-      <section className="lg:col-span-5 flex flex-col gap-y-4">
-        <DashboardChart totalSize={totalSize} />
-        <DashboardStats />
-      </section>
-      <section className="hidden h-full lg:block lg:col-span-5">
-        <RecentFiles documents={documents} />
-      </section>
-    </MainContainer>
-  );
+function HomePage() {
+  return redirect("/dashboard");
 }
-export default DashboardPage;
+export default HomePage;
